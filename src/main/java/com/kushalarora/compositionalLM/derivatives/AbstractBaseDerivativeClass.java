9094c4098d54003a4ba93ac4450b871741590e06
Kushal Arora <kushal18@gmail.com>
2015-07-11 15:55:20 -0400

    fixed the serializable issue

--- src/main/java/com/kushalarora/compositionalLM/derivatives/AbstractBaseDerivativeClass.java
+++ src/main/java/com/kushalarora/compositionalLM/derivatives/AbstractBaseDerivativeClass.java
@@ -2,10 +2,12 @@ package com.kushalarora.compositionalLM.derivatives;
 
 import com.kushalarora.compositionalLM.model.Model;
 
+import java.io.Serializable;
+
 /**
  * Created by karora on 6/30/15.
  */
-public class AbstractBaseDerivativeClass {
+public class AbstractBaseDerivativeClass implements Serializable{
     protected final Model model;
 
     public AbstractBaseDerivativeClass( Model model) {
