c43aa5eced1f7f0e03e384a95a3dcd6a3671f9da
Kushal Arora <kushal18@gmail.com>
2015-07-21 06:33:29 -0400

    flush memcached at constructor

--- src/main/java/com/kushalarora/compositionalLM/caching/MemcachedWrapper.java
+++ src/main/java/com/kushalarora/compositionalLM/caching/MemcachedWrapper.java
@@ -19,7 +19,7 @@ public abstract class MemcachedWrapper<K, V> extends CacheWrapper<K, V> {
     public MemcachedWrapper(Options op) throws IOException {
         memcachedClient =new MemcachedClient(
                 AddrUtil.getAddresses("localhost:3030"));
-        ;
+        memcachedClient.flush();
     }
 
     @Override
