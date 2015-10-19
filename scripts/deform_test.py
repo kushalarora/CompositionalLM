245f055ad399ffd25b4ffbe039c414e1a32a658c
Kushal Arora <arorak@dev-dsk-arorak-2a-i-87cba441.us-west-2.amazon.com>
2015-10-19 19:23:20 +0000

    adding deformation script

---
+++ scripts/deform_test.py
@@ -0,0 +1,11 @@
+import sys
+
+def disform_sentences(input_filename, output_filename):
+	infile = open(input_filename, 'r')
+	outfile = open(output_filename, 'w')
+	
+	
+
+
+if __name__ == "__main__":
+	disform_sentences(sys.arg1, sys.arg2)
