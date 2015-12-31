3cc6e5c927c0d5c3a4f94648a21a4c9f24ab1d52
Arora <arorak@amazon.com>
2015-12-30 16:05:46 -0800

    normalizing at the end. Using uniform distribution between -1 to 1

--- src/main/java/com/kushalarora/compositionalLM/model/Parameters.java
+++ src/main/java/com/kushalarora/compositionalLM/model/Parameters.java
@@ -33,9 +33,9 @@ public class Parameters implements IParameter<Sentence> {
         rng.setSeed(1032);
         this.dimensions = dimensions;
         this.vocabSize = vocabSize;
-        W = Nd4j.randn(dimensions, 2 * dimensions, rng);      // d X 2d matrix
-        u = Nd4j.randn(1, dimensions, rng);                   // row vector with d entries
-        X = Nd4j.randn(dimensions, vocabSize, rng);           // d X V matrix
+        W = Nd4j.rand(dimensions, 2 * dimensions, -1, 1, rng);      // d X 2d matrix
+        u = Nd4j.rand(1, dimensions, -1, 1, rng);                   // row vector with d entries
+        X = Nd4j.rand(dimensions, vocabSize, -1, 1, rng);           // d X V matrix
 
         INDArray normVec =  X.norm2(0);
         X = X.divRowVector(normVec);
@@ -110,8 +110,8 @@ public class Parameters implements IParameter<Sentence> {
         }
 
         X = X.subRowVector(X.mean(0));
-//        INDArray normVec =  X.norm2(0);
-//        X = X.divRowVector(normVec);
+        INDArray normVec =  X.norm2(0);
+        X = X.divRowVector(normVec);
         log.info("new X = \n {}", X);
     }
 }
