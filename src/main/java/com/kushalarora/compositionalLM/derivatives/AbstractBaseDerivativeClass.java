db461460b215bf19a2368fd568b658a657b4f4bc
Arora <arorak@amazon.com>
2016-01-09 12:28:06 -0800

    not normalizing X and updating at each cycle

--- src/main/java/com/kushalarora/compositionalLM/derivatives/AbstractBaseDerivativeClass.java
+++ src/main/java/com/kushalarora/compositionalLM/derivatives/AbstractBaseDerivativeClass.java
@@ -41,7 +41,7 @@ public abstract class AbstractBaseDerivativeClass<T extends IIndexedSized> imple
             numElements *= arr.shape()[i];
         }
         double norm2 = Nd4j.norm2(arr).getDouble(0);
-        double cutoff = 50;
+        double cutoff = 2;
         if (norm2/numElements > cutoff) {
             log.error("Clipping gradiant of shape {} for data:{}::{}. Norm = {}",
                     shape, data.getIndex(), data.getSize(),  norm2);
--- src/main/java/com/kushalarora/compositionalLM/optimizer/AbstractOptimizer.java
+++ src/main/java/com/kushalarora/compositionalLM/optimizer/AbstractOptimizer.java
@@ -111,6 +111,10 @@ public abstract class AbstractOptimizer<T extends IIndexedSized, D extends IDeri
                 batchScore += derivative.getScore();
             }
         }
+        // update param for this batch
+        D dAcc = getAccumulatedDerivative();
+        dAcc.mul(1.0 / batchSize);
+        updateParams(dAcc);
         return batchScore;
     }
 
@@ -223,11 +227,6 @@ public abstract class AbstractOptimizer<T extends IIndexedSized, D extends IDeri
                         epoch, trainFileIdx, estimatedTrainfileTime,
                                 cumlTrainScore / cumlTrainBatchSize);
             }   // end for trainList
-
-            // update param for this batch
-            D dAcc = getAccumulatedDerivative();
-            dAcc.mul(1.0 / cumlTrainBatchSize);
-            updateParams(dAcc);
 
             // clear accumulator and
             // re-initialize learing rate
