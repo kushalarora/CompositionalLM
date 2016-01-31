8092d90c0c97c6728022be127976a3641b9a2ad6
Arora <arorak@amazon.com>
2016-01-31 14:24:20 -0800

    rewrite of parallelizer

--- src/main/java/com/kushalarora/compositionalLM/utils/Parallelizer.java
+++ src/main/java/com/kushalarora/compositionalLM/utils/Parallelizer.java
@@ -9,10 +9,12 @@ import java.util.concurrent.Future;
 
 import com.google.common.base.Function;
 import com.kushalarora.compositionalLM.options.Options;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by arorak on 12/11/15.
  */
+@Slf4j
 public class Parallelizer {
     public Parallelizer(Options op, int blockSize) {
         this.blockSize = blockSize;
@@ -23,30 +25,26 @@ public class Parallelizer {
     private int blockSize;
     protected ExecutorService executor;
 
-    public <D> List<Future<List<D>>> parallelizer(final int start, final int end, final Function<Integer, D> parallizableFunc) {
+    public <D> List<Future<List<D>>> parallelizer(final int start, final int end,
+                                                  final Function<Integer, D> parallizableFunc,
+                                                  final int blockSize) {
         int length = end - start;
         // Contains one extra block. Do something about it.
-        final int blockNum = length / blockSize + 1;
+        int blockNum = length / blockSize;
+
+        if (blockNum < 1) {
+           /* log.error("blockNum is zero. " +
+                    "Start: " + start +
+                    " End: " + end  +
+                    " blockSize: " + blockSize);*/
+            blockNum = 1;
+        }
 
         List<Callable<List<D>>> callables = new ArrayList<Callable<List<D>>>();
         for (int i = 0; i < blockNum; i++) {
-            callables.add(new Callable<List<D>>() {
-
-                public List<D> call() throws Exception {
-                    List<D> dList = new ArrayList<D>();
-                    for (int j = blockStartIdx; j < blockStartIdx + blockSize && j < end; j++) {
-                        dList.add(parallizableFunc.apply(j));
-                    }
-                    return dList;
-                }
-
-                public Callable<List<D>> init(int blockIndex) {
-                    this.blockStartIdx = blockIndex;
-                    return this;
-                }
-
-                private int blockStartIdx;
-            }.init(start + i * blockSize));
+            int startIdx = start + i * blockSize;
+            int endIdx = (startIdx + blockSize < end) ? startIdx + blockSize : end;
+            callables.add(new CallableWithInit<D>(startIdx, endIdx, parallizableFunc));
         }
 
         try
@@ -56,6 +54,29 @@ public class Parallelizer {
         catch (InterruptedException e)
         {
             throw new RuntimeException(e);
+        }
+    }
+
+    public <D> List<Future<List<D>>> parallelizer(final int start, final int end, final Function<Integer, D> parallizableFunc) {
+        return parallelizer(start, end, parallizableFunc, blockSize);
+    }
+
+    public class CallableWithInit<D> implements Callable<List<D>> {
+        public int startIdx;
+        public int endIndex;
+        public Function<Integer, D> parallelizableFunc;
+        public CallableWithInit(int startIndex, int endIndex, Function<Integer, D> parallelizableFunc) {
+            this.startIdx = startIndex;
+            this.endIndex = endIndex;
+            this.parallelizableFunc = parallelizableFunc;
+        }
+
+        public List<D> call() throws Exception {
+            List<D> dList = new ArrayList<D>();
+            for (int j = startIdx; j < endIndex; j++) {
+                dList.add(parallelizableFunc.apply(j));
+            }
+            return dList;
         }
     }
 }
