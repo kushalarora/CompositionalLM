f4e9a43804cf7772f83e7a2a547ae53e787a0e22
Arora <arorak@amazon.com>
2016-02-21 00:11:56 -0800

    X normalized

--- src/main/java/com/kushalarora/compositionalLM/model/Parameters.java
+++ src/main/java/com/kushalarora/compositionalLM/model/Parameters.java
@@ -38,6 +38,10 @@ public class Parameters implements IParameter<Sentence> {
         W = Nd4j.rand(dimensions, 2 * dimensions, -1, 1, rng);      // d X 2d matrix
         u = Nd4j.rand(1, dimensions, -1, 1, rng);                   // row vector with d entries
         X = Nd4j.rand(dimensions, vocabSize, -1, 1, rng);           // d X V matrix
+        X = X.subRowVector(X.mean(0));
+        INDArray normVec =  X.norm2(0);
+        X = X.divRowVector(normVec);
+        log.info("new X = \n {}", X);
         this.op = op;
     }
 
@@ -115,17 +119,15 @@ public class Parameters implements IParameter<Sentence> {
             X = X.sub(X.mul(l2term));
         }
 
-/*
         X = X.subRowVector(X.mean(0));
         INDArray normVec =  X.norm2(0);
         X = X.divRowVector(normVec);
         log.info("new X = \n {}", X);
-*/
 
 
         log.info("$#Norm2 u : {}", Nd4j.norm2(u));
         log.info("$#Norm2 W : {}", Nd4j.norm2(W));
         log.info("$#Norm2 X : {}", Nd4j.norm2(X));
-        
+
     }
 }
